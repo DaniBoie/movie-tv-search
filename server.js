29849a49060b112a64805c80824a98d997850f1c
Daniel Ayala <dannyboy3385@gmail.com>
2020-09-18 11:47:37 -0700

    working react + express server

--- server.js
+++ server.js
@@ -2,12 +2,22 @@ const express = require('express')
 const app = express()
 const { join } = require('path')
 
-app.use(express.static(join(__dirname, 'public')))
+if (process.env.NODE_ENV  === 'production') {
+  app.use(express.static(join(__dirname, 'client', 'public')))
+}
+
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
 app.use(require('./routes'))
 
+if (process.env.NODE_ENV === 'production') {
+  app.get('/*', (req, res) => {
+    res.sendFile(join(__dirname, 'client', 'public', 'index.html'))
+  })
+}
+
 require('./db')
   .then(() => app.listen(process.env.PORT || 3001))
   .catch(err => console.log(err))
+
